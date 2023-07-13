d5c8e3e2160cff3e7de9eb3574e1169dc6b61294
Jack Fannon <jackepfannon@gmail.com>
2023-07-13 11:28:30 +0100

    h2root work with skofl

--- MCGenerator/h2root.sh
+++ MCGenerator/h2root.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-H2ROOT=/home/skofl/sklib_g77/skofl_16c/bin/h2root
+H2ROOT=/usr/local/sklib_gcc8/skofl-trunk/bin/h2root
 for FILE in `ls hbk/*.hbk`; do
 #for FILE in `ls hbk/163[5-9]*.hbk`; do
 #for FILE in `ls hbk/164*.hbk`; do
