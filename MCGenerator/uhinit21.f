1a42d0281d527578c819db180a59eab1473e9146
Jack Fannon <jackepfannon@gmail.com>
2023-08-17 10:04:16 +0100

    Changed uhinit parameters for old detector

--- MCGenerator/uhinit21.f
+++ MCGenerator/uhinit21.f
@@ -12,8 +12,8 @@ c     parameter (offset=36.379)
 c     parameter (slope=5.2230)
 c     S.Shima: Updating the calibrtion parameters using the 2021 data
 c     Aug. 29, 2021      
-      parameter (offset=1.12369)
-      parameter (slope=6.74495)
+      parameter (offset=-8.084)
+      parameter (slope=0.1915)
 
       hmin = offset/1000.
       hmax = (slope*4096.+offset)/1000.
